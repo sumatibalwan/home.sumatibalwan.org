b959646a8056224bf0d625d5a4c2469843481b3b
Coding 4 Girls <coding4girls@via-internet.de>
2024-11-25 21:02:17 +0100

    -

--- donation/get_exchange_rate.php
+++ donation/get_exchange_rate.php
@@ -1,24 +1,23 @@
 <?php
 
-// Replace with your Fixer.io API Key
-$apiKey = '3ff60558007b9fb625bb27be6051adc1';
+require_once __DIR__.'/../vendor/autoload.php';
 
-// Default currency to USD
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->safeLoad();
+
+$apiKey = $_SERVER['API_TOKEN'];
+
 $baseCurrency = 'USD';
 
 if (isset($_GET['currency'])) {
     $targetCurrency = $_GET['currency'];
 
-    // API URL for Fixer.io
     $url = "http://data.fixer.io/api/latest?access_key=$apiKey&symbols=$baseCurrency,$targetCurrency";
 
-    // Fetch exchange rates from Fixer.io
     $response = file_get_contents($url);
     $data = json_decode($response, true);
 
-    // Check if the response is valid
     if ($data['success']) {
-        // Calculate the exchange rate
         $rate = $data['rates'][$baseCurrency] / $data['rates'][$targetCurrency];
         echo json_encode(['success' => true, 'rate' => $rate]);
     } else {
